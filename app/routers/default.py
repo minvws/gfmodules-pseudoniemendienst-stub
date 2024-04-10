a36c067a152972eefd9c4e5b82be3dfce9239981
Joshua Thijssen <jthijssen@noxlogic.nl>
2024-04-10 11:16:03 +0200

    Initial skeleton

---
+++ app/routers/default.py
@@ -0,0 +1,36 @@
+import logging
+
+import json
+from pathlib import Path
+from fastapi import APIRouter, Response
+
+logger = logging.getLogger(__name__)
+router = APIRouter()
+
+# https://www.patorjk.com/software/taag/#p=display&f=Doom&t=Skeleton
+LOGO = r"""
+______                   _                             
+| ___ \                 | |                            
+| |_/ /__  ___ _   _  __| | ___  _ __  _   _ _ __ ___  
+|  __/ __|/ _ \ | | |/ _` |/ _ \| '_ \| | | | '_ ` _ \ 
+| |  \__ \  __/ |_| | (_| | (_) | | | | |_| | | | | | |
+\_|  |___/\___|\__,_|\__,_|\___/|_| |_|\__, |_| |_| |_|
+                                        __/ |          
+                                       |___/           
+
+"""
+
+
+@router.get("/")
+def index() -> Response:
+    content = LOGO
+
+    try:
+        with open(Path(__file__).parent.parent.parent / 'version.json', 'r') as file:
+            data = json.load(file)
+            content += "\nVersion: %s\nCommit: %s" % (data['version'], data['git_ref'])
+    except BaseException as e:
+        content += "\nNo version information found"
+        logger.info("Version info could not be loaded: %s" % e)
+
+    return Response(content)
