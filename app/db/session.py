ab4621cf9ff1ace1ee7f12b79f33e91a878b2a8b
Joshua Thijssen <jthijssen@noxlogic.nl>
2024-06-19 10:07:21 +0200

    Updated db session

--- app/db/session.py
+++ app/db/session.py
@@ -7,15 +7,36 @@ from app.db.repository import RepositoryBase
 from app.db.decorator import repository_registry
 from app.db.models import Base
 
+"""
+This module contains the DbSession class, which is a context manager that provides a session to interact with 
+the database. It also provides methods to add and delete resources from the session, and to commit or rollback the
+current transaction.
+
+Usage:
+
+    with DbSession(engine) as session:
+        repo = session.get_repository(MyModel)
+        repo.find_all()
+        session.add_resource(MyModel())
+        session.commit()       
+"""
+
 
 class DbSession:
     def __init__(self, engine: Engine) -> None:
-        self.session = Session(engine, expire_on_commit=False)
+        self._engine = engine
 
     def __enter__(self) -> 'DbSession':
+        """
+        Create a new session when entering the context manager
+        """
+        self.session = Session(self._engine, expire_on_commit=False)
         return self
 
     def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        """
+        Close the session when exiting the context manager
+        """
         self.session.close()
 
     def get_repository(self, model: Type[Base]) -> RepositoryBase:
@@ -64,4 +85,3 @@ class DbSession:
         :return:
         """
         self.session.rollback()
-
