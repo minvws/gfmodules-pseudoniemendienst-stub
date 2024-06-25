34d1ead7fc7d9244eb1a4023acd356e80d65fe99
Joshua Thijssen <jthijssen@noxlogic.nl>
2024-06-25 08:41:25 +0200

    Added retry mechanism

--- app/db/db.py
+++ app/db/db.py
@@ -1,8 +1,9 @@
 import logging
 
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, StaticPool
 from sqlalchemy.orm import Session
 
+from app.config import get_config
 from app.db.session import DbSession
 from app.db.models import Base
 
@@ -12,7 +13,22 @@ logger = logging.getLogger(__name__)
 class Database:
     def __init__(self, dsn: str):
         try:
-            self.engine = create_engine(dsn, echo=False)
+            if "sqlite://" in dsn:
+                self.engine = create_engine(
+                    dsn,
+                    connect_args={'check_same_thread': False},      # This + static pool is needed for sqlite in-memory tables
+                    poolclass=StaticPool
+                )
+            else:
+                config = get_config()
+                self.engine = create_engine(
+                    dsn,
+                    echo=False,
+                    pool_pre_ping=config.database.pool_pre_ping,
+                    pool_recycle=config.database.pool_recycle,
+                    pool_size=config.database.pool_size,
+                    max_overflow=config.database.max_overflow
+                )
         except BaseException as e:
             logger.error("Error while connecting to database: %s", e)
             raise e
