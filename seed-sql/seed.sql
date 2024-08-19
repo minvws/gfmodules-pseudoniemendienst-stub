7c34ce4ff89565f06e3dd0e3d42480655a781392
Gerbrand Bosch <gerbrandb@gmail.com>
2024-08-19 14:50:28 +0200

    Update provider ID to metadata URA

--- seed-sql/seed.sql
+++ seed-sql/seed.sql
@@ -5,5 +5,5 @@ INSERT INTO public.pseudonyms (id, hashed_bsn, provider, pseudonym) VALUES ('5c8
 ---- Localisation Service
 INSERT INTO public.pseudonyms (id, hashed_bsn, provider, pseudonym) VALUES ('afb82a65-f6ce-4358-910a-2f617d9179c1', '2b826afa1c71f571a007dad61ce9fb9b63a24a328aa4ac009484a198ff565c51', 'dcc2217b-600e-4587-b74a-6b04bf3a882b', '5afda8a1-dce9-43f8-b1af-c4dc765e7e17');
 
----- Metadata Service
-INSERT INTO public.pseudonyms (id, hashed_bsn, provider, pseudonym) VALUES ('09a30098-81b0-4ded-9a45-2ca71b5192f5', '2b826afa1c71f571a007dad61ce9fb9b63a24a328aa4ac009484a198ff565c51', '28f781a3-58c8-5e81-998c-28c71c25ce2b', 'bbf54282-3d58-4bed-b27b-dd4e1b85ec08');
+---- Metadata Service (Ura:23665292)
+INSERT INTO public.pseudonyms (id, hashed_bsn, provider, pseudonym) VALUES ('47233fb0-734b-41d3-95e7-b7696cfa00c1', '2b826afa1c71f571a007dad61ce9fb9b63a24a328aa4ac009484a198ff565c51', '23665292', 'bbf54282-3d58-4bed-b27b-dd4e1b85ec08');
